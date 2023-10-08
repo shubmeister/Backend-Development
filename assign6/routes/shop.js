66caa37491fc88e2b2a5d4ece513857332d98ae8
shubham.negi <shubhamnegi@niralnetworks.in>
2023-10-08 21:44:17 +0530

    model added
    model added

---
+++ assign6/routes/shop.js
@@ -0,0 +1,13 @@
+
+const express=require("express");
+ 
+const path=require('path')
+const route=express.Router();
+
+route.get('/',(req,res,next)=>{
+
+   // res.send('<h1>List of product</h1>')
+   res.sendFile(path.join(__dirname, '../', 'views','shop.html'))
+})
+
+module.exports=route;
